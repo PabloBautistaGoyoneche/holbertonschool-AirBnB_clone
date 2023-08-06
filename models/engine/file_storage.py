c6786f70347e76ced259951c9288dea6cfa71f23
PabloBautistaGoyoneche <pablo.bautista.mkt@gmail.com>
2023-08-06 17:00:23 -0500

    Correction of comments and code in the file file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,61 +1,47 @@
 import json
-import models
+import os
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 
 class FileStorage:
     __file_path = 'file.json'
-    __objects = {}  # Coloca aquí el atributo __objects como se especificó
+    __objects = {} 
 
     def all(self):
         return self.__objects
-
+    
     def new(self, obj):
-        """Agrega una nueva instancia al diccionario __objects"""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        """ Add a new instance to the __objects dictionary """
+        if obj:
+            key = '{}.{}'.format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
-        """Guarda las instancias en un archivo JSON"""
-        objects_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(objects_dict, file)
+        """ Save the instances in a JSON file """
+        data = {}
+        for k, v in self.__objects.items():
+            data[k] = v.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(data, f)
 
     def reload(self):
-        """Carga las instancias desde el archivo JSON"""
-        file = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review
-        }
+        """ Load the instances from the JSON file """
+        classes = {"BaseModel": BaseModel}
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                objects_dict = json.load(file)
-                for obj_id, obj_data in objects_dict.items():
-                    class_name = obj_data.pop('__class__', None)
-                    if class_name and class_name in BaseModel.__subclasses__():
-                        cls = BaseModel.__subclasses__[class_name]
-                        obj = cls(**obj_data)
-                        self.__objects[obj_id] = obj
-        except FileNotFoundError: 
+            with open(self.__file_path, 'r') as f:
+                data = f.read()
+                data = json.loads(data)
+                for k, v in data.items():
+                    self.__objects[k] = classes[v["__class__"]](**v)
+        except FileNotFoundError:
             pass
 
-    def delete(self, obj=None):
-        """Elimina un objeto de __objects si está presente"""
+    def delete (self, obj=None):
+        """ Removes an object from __objects if it is present """
         if obj:
             key = f"{obj.__class__.__name__}.{obj.id}"
-            self.__objects.pop(key, None)  # Usa pop para eliminar de __objects
+            self.__objects.pop(key, None) 
 
     def close(self):
-        """Llama al método save() para guardar los cambios antes de terminar el programa"""
+        """ Call the save() method to save the changes before terminating the program. """
         self.save()
 
