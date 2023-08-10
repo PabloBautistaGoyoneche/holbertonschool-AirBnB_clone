ed4a3e911ae12e8fdb6776ff660c491050bd4466
Alexander1455 <alexander21111121@gmail.com>
2023-08-10 16:47:11 -0700

    Updating and improving the file test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,29 +1,80 @@
 #!/usr/bin/python3
-"""We test te class Amenity and all its functions"""
+"""
+Unittest to test state class
+"""
 import unittest
-from datetime import datetime
+import inspect
+import json
+import os
+import pycodestyle
+from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
-class TestBase(unittest.TestCase):
-    """The test class to work in unicode"""
+class TestFileStorageDocs(unittest.TestCase):
+    """Tests for documentation of class"""
 
-    def testSet(self):
-        """Check if you can generate an instance"""
-        self.C = Amenity()
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.amenity_funcs = inspect.getmembers(Amenity, inspect.isfunction)
 
-    def testExist(self):
-        """A test that check if the attributes exists in the class"""
-        A1 = Amenity()
-        self.assertTrue(hasattr(A1, "id"))
-        self.assertTrue(hasattr(A1, "created_at"))
-        self.assertTrue(hasattr(A1, "updated_at"))
-        self.assertTrue(hasattr(A1, "name"))
+    def test_conformance_class(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def testUser(self):
-        """A test to check if all values are the correct type"""
-        A2 = Amenity()
-        self.assertIsInstance(A2.id, str)
-        self.assertIsInstance(A2.created_at, datetime)
-        self.assertIsInstance(A2.updated_at, datetime)
-        self.assertIsInstance(A2.name, str)
+    def test_conformance_test(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.\
+            check_files(['tests/test_models/test_amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_module_docstr(self):
+        """Tests for docstring"""
+        self.assertTrue(len(Amenity.__doc__) >= 1)
+
+    def test_class_docstr(self):
+        """Tests for docstring"""
+        self.assertTrue(len(Amenity.__doc__) >= 1)
+
+    def test_func_docstr(self):
+        """Tests for docstrings in all functions"""
+        for func in self.amenity_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
+
+class TestState(unittest.TestCase):
+
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(Amenity().__class__, BaseModel), True)
+
+    def test_attr_str(self):
+        self.assertEqual(type(Amenity().name), str)
+
+    def test_has_attributes_in_to_dict(self):
+        """check if attr is in to_dict"""
+        amenity = Amenity()
+        amenity.name = "garden"
+        self.assertTrue('id' in amenity.to_dict())
+        self.assertTrue('created_at' in amenity.to_dict())
+        self.assertTrue('updated_at' in amenity.to_dict())
+        self.assertTrue('name' in amenity.to_dict())
+
+    def test_save(self):
+        amenity = Amenity()
+        amenity.save()
+        self.assertNotEqual(amenity.created_at, amenity.updated_at)
+
+    def test_to_dict(self):
+        amenity = Amenity()
+        self.assertTrue(dict, type(amenity.to_dict))
+        self.assertEqual('to_dict' in dir(amenity), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
